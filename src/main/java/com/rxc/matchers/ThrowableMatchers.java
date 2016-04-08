85f2958226b3b51c15cade20f7565e7740c8667a
eanlom <ean.lombardo@asynchrony.com>
2016-04-08 18:49:02 -0500

    added javadoc for ThrowableMatchers

--- src/main/java/com/rxc/matchers/ThrowableMatchers.java
+++ src/main/java/com/rxc/matchers/ThrowableMatchers.java
@@ -6,6 +6,11 @@ import org.hamcrest.Matcher;
 
 public class ThrowableMatchers {
 
+    /**
+     * A {@link Matcher} that matches a Throwable with a message that matches a given matcher
+     * @param stringMatcher The matcher to check the Throwable message against
+     * @return the matcher
+     */
     public static Matcher<Throwable> hasMessageThat(final Matcher<String> stringMatcher){
        return new FeatureMatcher<Throwable, String>(stringMatcher,"message","message") {
            @Override
@@ -15,6 +20,11 @@ public class ThrowableMatchers {
        };
     }
 
+    /**
+     * A {@link Matcher} that matches a Throwable with a cause that matches a given matcher
+     * @param throwableMatcher The matcher to check the Throwable cause against
+     * @return the matcher
+     */
     public static Matcher<Throwable> hasCauseThat(final Matcher<Throwable> throwableMatcher){
         return new FeatureMatcher<Throwable, Throwable>(throwableMatcher,"cause","cause") {
             @Override
